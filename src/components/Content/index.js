d20fcb66f6c5e9f6f4f2c7a44e6eb4ae19b662e5
M Haidar Hanif <git@mhaidarhanif.com>
2019-01-08 16:41:46 +0700

    Use styled together with style object

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,19 +1,23 @@
-import React, { Component } from "react"
+import React from "react"
 
-class Content extends Component {
-  render() {
-    return (
-      <main
-        style={{
-          color: "yellow",
-          background: "black",
-          padding: "10px"
-        }}
-      >
-        <p>Learning React with Ancalagon together is better #McQueenYaQueen</p>
-      </main>
-    )
-  }
+import styled from "styled-components"
+
+const mainStyle = {
+  color: "yellow",
+  background: "black",
+  padding: "10px"
 }
+
+const Paragraph = styled.p`
+  font-size: 20px;
+`
+
+const Content = () => (
+  <main style={mainStyle}>
+    <Paragraph>
+      Learning React with Ancalagon together is better #McQueenYaQueen
+    </Paragraph>
+  </main>
+)
 
 export default Content
