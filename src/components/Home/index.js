357bcf45d3e512ad3f2ff381cb0266de010228e3
M Haidar Hanif <git@mhaidarhanif.com>
2019-01-08 17:24:25 +0700

    Refactor content style into Page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,25 +4,17 @@ import { Button } from "reactstrap"
 
 import Page from "../Page"
 
-const mainStyle = {
-  color: "yellow",
-  background: "black",
-  padding: "10px"
-}
-
 const Paragraph = styled.p`
   font-size: 20px;
 `
 
 const Content = () => (
   <Page>
-    <main style={mainStyle}>
-      <Paragraph>
-        Learning React with Ancalagon together is better #McQueenYaQueen
-      </Paragraph>
-      <Button color="primary">Get Started</Button>
-      <Button color="danger">Danger</Button>
-    </main>
+    <Paragraph>
+      Learning React with Ancalagon together is better #McQueenYaQueen
+    </Paragraph>
+    <Button color="primary">Get Started</Button>
+    <Button color="danger">Danger</Button>
   </Page>
 )
 
--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -3,10 +3,16 @@ import React from "react"
 import Header from "../Header"
 import Footer from "../Footer"
 
+const mainStyle = {
+  color: "yellow",
+  background: "black",
+  padding: "10px"
+}
+
 const Page = props => (
   <div>
     <Header />
-    {props.children}
+    <main style={mainStyle}>{props.children}</main>
     <Footer />
   </div>
 )
