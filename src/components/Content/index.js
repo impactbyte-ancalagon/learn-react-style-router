e9d876a44c05899355e7cc6305cdac296ccc3551
M Haidar Hanif <git@mhaidarhanif.com>
2019-01-08 16:27:15 +0700

    Implement some components

---
+++ src/components/Content/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+class Content extends Component {
+  render() {
+    return (
+      <main>
+        <p>Learning React with Ancalagon togeter is better #McQueenYaQueen</p>
+      </main>
+    )
+  }
+}
+
+export default Content
