9ca7da4cf9187aab717b9a6370a2408b8fb20a10
M Haidar Hanif <git@mhaidarhanif.com>
2019-01-08 16:34:44 +0700

    Style with styled-components

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,35 @@
 import React, { Component } from "react"
 
+import styled from "styled-components"
+
+const NavigationBar = styled.header`
+  font-size: 1.5em;
+  text-align: center;
+`
+
+const Title = styled.h1`
+  color: palevioletred;
+`
+
+const NavigationLinks = styled.ul`
+  text-decoration: none;
+  li {
+    display: inline-block;
+    margin-right: 10px;
+  }
+`
+
 class Header extends Component {
   render() {
     return (
-      <header>
-        <h1>Learn React</h1>
-      </header>
+      <NavigationBar>
+        <Title>Learn React</Title>
+        <NavigationLinks>
+          <li>Home</li>
+          <li>About</li>
+          <li>Random</li>
+        </NavigationLinks>
+      </NavigationBar>
     )
   }
 }
